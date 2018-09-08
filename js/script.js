806523c92bbe9fda685a014ed730148d881d2d2a
Matilda-Kric <41109245+Matilda-Kric@users.noreply.github.com>
2018-09-08 16:18:23 +0500

    Исправления

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+	var search = document.querySelector(".search-hotels-button");
+	var popup  = document.querySelector(".search-form");
+	var arrivalDate = popup.querySelector("[name=arrival-date]");
+	var departureDate = popup.querySelector("[name=departure-date]");
+	var adults = popup.querySelector("[name=adults]");
+
+	search.addEventListener("click", function(evt){
+		evt.preventDefault();
+		popup.classList.toggle("search-form-close");
+		arrivalDate.focus();
+	});			
+
+	popup.addEventListener("submit", function (evt) {
+    if (!arrivalDate.value || !departureDate.value || !adults.value) {
+    	evt.preventDefault();
+     	popup.classList.remove("search-form-error");
+     	popup.offsetWidth = popup.offsetWidth;
+     	popup.classList.add("search-form-error");
+    }
+  });
